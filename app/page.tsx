17a0c93f505761a205b006503b15fd3d9482a05f
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-05-22 15:01:30 +0500

    feat:add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>hi there how are you doin</h1>
-      <Button variant={"outline"} className=" text-white">
-        Click Me
-      </Button>
+    <div className="relative w-full overflow-hidden">
+      <HeroSection />
     </div>
   );
 }
