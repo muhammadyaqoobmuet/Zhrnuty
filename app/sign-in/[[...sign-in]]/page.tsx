7a9e0937eb637b722582c835edee2e22f7f55853
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-06-03 19:11:21 +0500

    feat: update sign-in and sign-up pages to use structured layout with dynamic height

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn oauthFlow="popup" />;
+  return (
+    <div className="min-h-[90vh]">
+      <div className="container pt-20 mx-auto flex flex-col items-center justify-center gap-4">
+        <SignIn oauthFlow="redirect" />
+      </div>
+    </div>
+  );
 }
 export const dynamic = "force-dynamic"; // This page should always be dynamic
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,14 @@
+import BgGradient from "@/components/common/bg-gradient";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp oauthFlow="popup" />;
+  return (
+    <div className="min-h-[80vh]">
+      <BgGradient />
+      <div className="container py-2 pb-10 mx-auto flex flex-col items-center justify-center gap-4">
+        <SignUp oauthFlow="popup" />
+      </div>
+    </div>
+  );
 }
 export const dynamic = "force-dynamic"; // This page should always be dynamic
