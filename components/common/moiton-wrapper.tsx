7c7d9cc6b731ac7aae3afebb98a628df1622b2d5
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-06-03 19:13:05 +0500

    feat: add Motion wrapper component for enhanced motion support across HTML elements

---
+++ components/common/moiton-wrapper.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { motion } from "motion/react";
+// JUST TO MAKE SURE WE DONT HAVE TO CONVERT SERVER SIDE COMPONETS INTO CLIENT SIDE JUST CZ OF MOITON IT IS A CLIENT SIDE LIBRARY
+
+// Basic HTML Elements
+export const MotionDiv = motion.div;
+export const MotionSection = motion.section;
+export const MotionPara = motion.p;
+export const MotionSpan = motion.span;
+export const MotionArticle = motion.article;
+export const MotionAside = motion.aside;
+export const MotionMain = motion.main;
+export const MotionHeader = motion.header;
+export const MotionFooter = motion.footer;
+export const MotionNav = motion.nav;
+
+// Headings
+export const MotionH1 = motion.h1;
+export const MotionH2 = motion.h2;
+export const MotionH3 = motion.h3;
+export const MotionH4 = motion.h4;
+export const MotionH5 = motion.h5;
+export const MotionH6 = motion.h6;
+
+// Text Elements
+export const MotionStrong = motion.strong;
+export const MotionEm = motion.em;
+export const MotionSmall = motion.small;
+export const MotionMark = motion.mark;
+export const MotionCode = motion.code;
+export const MotionPre = motion.pre;
+export const MotionBlockquote = motion.blockquote;
+
+// Lists
+export const MotionUl = motion.ul;
+export const MotionOl = motion.ol;
+export const MotionLi = motion.li;
+export const MotionDl = motion.dl;
+export const MotionDt = motion.dt;
+export const MotionDd = motion.dd;
+
+// Links and Media
+export const MotionA = motion.a;
+export const MotionImg = motion.img;
+export const MotionVideo = motion.video;
+export const MotionAudio = motion.audio;
+export const MotionCanvas = motion.canvas;
+export const MotionSvg = motion.svg;
+
+// Form Elements
+export const MotionForm = motion.form;
+export const MotionInput = motion.input;
+export const MotionTextarea = motion.textarea;
+export const MotionButton = motion.button;
+export const MotionSelect = motion.select;
+export const MotionOption = motion.option;
+export const MotionLabel = motion.label;
+export const MotionFieldset = motion.fieldset;
+export const MotionLegend = motion.legend;
+
+// Tables
+export const MotionTable = motion.table;
+export const MotionThead = motion.thead;
+export const MotionTbody = motion.tbody;
+export const MotionTfoot = motion.tfoot;
+export const MotionTr = motion.tr;
+export const MotionTh = motion.th;
+export const MotionTd = motion.td;
+export const MotionCaption = motion.caption;
+
+// Interactive Elements
+export const MotionDetails = motion.details;
+export const MotionSummary = motion.summary;
+export const MotionDialog = motion.dialog;
+
+// Semantic Elements
+export const MotionFigure = motion.figure;
+export const MotionFigcaption = motion.figcaption;
+export const MotionAddress = motion.address;
+export const MotionTime = motion.time;
+
+// Generic Container Elements
+export const MotionHr = motion.hr;
+export const MotionBr = motion.br;
