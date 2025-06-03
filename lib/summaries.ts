234ce9713ccf4c4d6297617c3ce7c8ffa31d67e5
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-06-03 11:26:24 +0500

    feat: implement getSummariesById function to retrieve summary details by ID

--- lib/summaries.ts
+++ lib/summaries.ts
@@ -16,3 +16,38 @@ export async function getSummaries(userId: string) {
     }
   }
 }
+export async function getSummariesById(id: string) {
+  try {
+    // get summaries
+    const sql = await getDbConnection();
+
+    const [summary] = await sql`
+    SELECT
+      id,
+      user_id,
+      title,
+      original_file_url,
+      summary_text,
+      
+      created_at,
+      updated_at,
+      file_name,
+      LENGTH(summary_text) - LENGTH(REPLACE(summary_text, ' ', '')) + 1 as word_count
+    FROM
+      pdf_summaries
+    WHERE
+      id = ${id};
+  `;
+    if (!summary) {
+      throw new Error(" No Summaries Found");
+    }
+    return summary;
+  } catch (error) {
+    console.error(
+      error instanceof Error
+        ? error.message
+        : "something went wrong fetching summaries"
+    );
+    return null;
+  }
+}
