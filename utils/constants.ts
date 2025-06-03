2bee4753ae6e1872d1942b0459a14b4c2fe1407b
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-06-03 19:09:57 +0500

    feat: add motion properties for animations in constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,41 @@
+// ALL: moiton properties for animations
+export const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+      delayChildren: 0.1,
+    },
+  },
+};
+export const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    transition: {
+      type: "spring",
+      damping: 15,
+      stiffness: 50,
+      duration: 0.8,
+    },
+  },
+};
+
+export const buttonVariants = {
+  scale: 1.05,
+  transition: {
+    type: "spring",
+    stiffness: 300,
+    damping: 10,
+  },
+};
+export const listVariants = {
+  hidden: { opacity: 0, x: -30 },
+  visible: { opacity: 1, x: 0 },
+  transition: {
+    type: "spring",
+    stiffness: 100,
+    damping: 20,
+  },
+};
